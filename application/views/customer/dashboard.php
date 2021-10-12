79dcf8022bf8db151dfbb9b07174fd574283a90b
Awan Satria Ngalam <80205170+awansatriangalam@users.noreply.github.com>
2021-10-13 04:46:59 +0700

    link medsos

--- application/views/customer/dashboard.php
+++ application/views/customer/dashboard.php
@@ -53,16 +53,16 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/microsoft.svg" alt="..." /></a>
+                        <a href="https://www.microsoft.com/id-id"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/microsoft.svg" alt="..." /></a>
                     </div>
                     <div class="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/google.svg" alt="..." /></a>
+                        <a href="https://www.google.com/?hl=id"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/google.svg" alt="..." /></a>
                     </div>
                     <div class="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/facebook.svg" alt="..." /></a>
+                        <a href="https://id-id.facebook.com/"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/facebook.svg" alt="..." /></a>
                     </div>
                     <div class="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/ibm.svg" alt="..." /></a>
+                        <a href="https://www.ibm.com/id-en"><img class="img-fluid img-brand d-block mx-auto" src="<?php echo base_url('assets/assets_homepage/') ?>assets/img/logos/ibm.svg" alt="..." /></a>
                     </div>
                 </div>
             </div>
--- application/views/templates_customer/footer.php
+++ application/views/templates_customer/footer.php
@@ -4,9 +4,9 @@
                 <div class="row align-items-center">
                     <div class="col-lg-4 text-lg-start">Copyright &copy; Web Perumahan 2021</div>
                     <div class="col-lg-4 my-3 my-lg-0">
-                        <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-light btn-social mx-2" href="https://twitter.com/Twitter?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-light btn-social mx-2" href="https://id-id.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-light btn-social mx-2" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
                     </div>
                     <div class="col-lg-4 text-lg-end">
                         <a class="link-light text-decoration-none me-3" href="#!">Kebijakan Privasi</a>
