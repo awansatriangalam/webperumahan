4762d03498b5adf35d9b78690f9046543ccf92cd
Muhammad Ashif Sa'bani <muhashif8@gmail.com>
2021-10-14 11:41:52 +0700

    tentang

--- application/views/customer/tentang.php
+++ application/views/customer/tentang.php
@@ -3,7 +3,7 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Tentang Kami</h2>
-                    <h3 class="section-subheading text-muted">Informasi Mengenai Website Kami</h3>
+                    <h3 class="section-subheading text-muted">Informasi Mengenai Perusahaan Kami</h3>
                 </div>
                 <ul class="timeline">
                     <li>
@@ -11,49 +11,49 @@
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4>2009-2011</h4>
-                                <h4 class="subheading">Our Humble Beginnings</h4>
+                                <h4 class="subheading">Awal Mula Terbentuk</h4>
                             </div>
-                            <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div class="timeline-body"><p class="text-muted">Kami memulai karir ini berawal pada tahun 2009 banyak brosur bertebaran yang berisi promosi-promosi sebuah rumah. Nah, bermula pada si Paijo yang membawa brosur tentang perumahan, dari itu si Adi berpikir dan mengemukakan pendapatnya tentang membuat suatu website untuk lebih memudahkan orang dalam memasarkan perumahannya.</p></div>
                         </div>
                     </li>
                     <li class="timeline-inverted">
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/2.jpg" alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>March 2011</h4>
-                                <h4 class="subheading">An Agency is Born</h4>
+                                <h4>Maret 2011</h4>
+                                <h4 class="subheading">Lahirlah Sebuah Website Ini</h4>
                             </div>
-                            <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div class="timeline-body"><p class="text-muted">Maret tanggal 25 2011, awal mula kami meng-online kan website ini.Website yang dibuat oleh tim yang terdiri dari 3 orang yaitu Adia,Paijo, dan Basit.Mulanya website ini tidak sebegitu ramai karena orang-orang masih memakai cara lama, akan tetapi lama kemudian orang-orang sadar akan teknologi.</p></div>
                         </div>
                     </li>
                     <li>
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>December 2015</h4>
-                                <h4 class="subheading">Transition to Full Service</h4>
+                                <h4>Desember 2015</h4>
+                                <h4 class="subheading">Pelayanan Dengan Transaksi Yang Sudah Up To Date</h4>
                             </div>
-                            <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div class="timeline-body"><p class="text-muted">Desember 2015 merupakan bulan yang indah untuk website ini, dikarenakan pada bulan ini website Adhirekayasha City untuk pertama kalinya bisa melayanai transaksi dengan bergaya modern yang sudah update seperti khalayak-khalayak website luar negeri pada umumnya.</p></div>
                         </div>
                     </li>
                     <li class="timeline-inverted">
                         <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="..." /></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
-                                <h4>July 2020</h4>
-                                <h4 class="subheading">Phase Two Expansion</h4>
+                                <h4>Juli 2020</h4>
+                                <h4 class="subheading">Awal Mula Ekspansi Besar-Besaran</h4>
                             </div>
-                            <div class="timeline-body"><p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div class="timeline-body"><p class="text-muted">Pada Juli 2020 tanggal 12 lebih tepatnya merupakan sebuah pergerakan besar yang dilakukan oleh Adhirekayasha City ini.Mengapa tidak, karena pada tanggal itu banyak perusahaan-perusahaan besar yang ingin mengajak kerja sama dengan kami hingga kami harus bisa melakukan banyak perubahan terhadap Perusahaan kami ini.</p></div>
                         </div>
                     </li>
                     <li class="timeline-inverted">
                         <div class="timeline-image">
                             <h4>
-                                Be Part
+                                Mari Menjadi
                                 <br />
-                                Of Our
+                                Bagian Dari
                                 <br />
-                                Story!
+                                Cerita Kami !
                             </h4>
                         </div>
                     </li>
