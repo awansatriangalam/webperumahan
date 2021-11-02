fe7081a7d6a9dc1ab61d4d4d8b2ba976348c9f28
Muhammad Ashif Sa'bani <muhashif8@gmail.com>
2021-11-02 14:05:03 +0700

    konek pesan cust

--- application/controllers/customer/kontak.php
+++ application/controllers/customer/kontak.php
@@ -19,16 +19,23 @@ class Kontak extends CI_Controller{
                 $nama               = $this->input->post('nama');
                 $email              = $this->input->post('email');
                 $no_tlp             = $this->input->post('no_tlp');
-                $pesan              = $this->input->post('pesan');
+                $pesan_              = $this->input->post('pesan_');
 
                 $data = array(
                     'nama'          => $nama,
                     'email'         => $email,
                     'no_tlp'        => $no_tlp,
-                    'pesan'          => $pesan,
+                    'pesan_'         => $pesan_,
                 );
 
                 $this->rental_model->insert_data($data, 'pesan');
+                $this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible
+                    fade show" role="alert"> Data Berhasil Ditambahkan !.
+                <button type="button" class="close" data-dismiss="alert"
+                    aria-label="Close"
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>');
                 redirect('customer/kontak'); 
             }
         }
@@ -38,7 +45,7 @@ class Kontak extends CI_Controller{
             $this->form_validation->set_rules('nama','Nama','required');
             $this->form_validation->set_rules('email','Alamat Email','required');
             $this->form_validation->set_rules('no_tlp','No Telepon','required');
-            $this->form_validation->set_rules('pesan','Pesan','required');
+            $this->form_validation->set_rules('pesan_','Pesan','required');
         }
 }
 
