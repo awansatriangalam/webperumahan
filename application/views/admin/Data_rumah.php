f04d88cb2fa29e2353a3c320e11e6ae2501a9704
Awan Satria Ngalam <80205170+awansatriangalam@users.noreply.github.com>
2021-11-04 05:14:33 +0700

    Update Data_rumah.php

--- application/views/admin/Data_rumah.php
+++ application/views/admin/Data_rumah.php
@@ -9,7 +9,7 @@
 
             <?php echo $this->session->flashdata('pesan') ?>
 
-          <table class="table table-hover table-striped table-responsive table-bordered">
+          <table class="table table-hover table-striped table-responsive ">
               <thead>
                   <tr>
                     <th>No</th>
@@ -18,15 +18,6 @@
                     <th>Type</th>
                     <th>Luas Bangunan</th>
                     <th>Status</th>
-                    <th>Kamar Tidur</th>
-                    <th>Kamar Mandi</th>                   
-                    <th>Ruangan</th>
-                    <th>Lantai</th>
-                    <th>Garasi</th>
-                    <th>Deskripsi</th>
-                    <th>Alamat</th>
-                    <th>Kota</th>
-                    <th>Kode Pos</th>
                     <th>Harga</th>
                     <th>Aksi</th>
                   </tr>
@@ -38,29 +29,20 @@
                             <tr>
                                 <td><?php echo $no++ ?></td>
                                 <td>
-                                    <img width="75px" src="<?php echo base_url().'assets/upload/' .$rm->gambar ?>">
+                                    <img width="100px" src="<?php echo base_url().'assets/upload/' .$rm->gambar ?>">
                                 </td>
                                 <td><?php echo $rm->nama_rumah ?></td>
                                 <td><?php echo $rm->kode_type ?></td>
                                 <td><?php echo $rm->luas_bangunan ?></td>
                                 <td><?php 
                                 if ($rm->status == "0"){
-                                    echo "<span class='badge badge-danger'> Telah Diisi
+                                    echo "<span class='badge badge-danger'> Diisi
                                     </span>";
                                 }else{
                                     echo "<span class='badge badge-primary'> Tersedia
                                     </span>";
                                 }
                                 ?></td>
-                                <td><?php echo $rm->kamar_tidur ?></td>
-                                <td><?php echo $rm->kamar_mandi ?></td>
-                                <td><?php echo $rm->ruangan ?></td>
-                                <td><?php echo $rm->lantai ?></td>  
-                                <td><?php echo $rm->garasi ?></td> 
-                                <td><?php echo $rm->deskripsi ?></td>
-                                <td><?php echo $rm->alamat ?></td>
-                                <td><?php echo $rm->kota ?></td>
-                                <td><?php echo $rm->kode_pos ?></td>
                                 <td><?php echo $rm->harga ?></td>
                                 <td>
                                     <a href="<?php echo base_url('admin/data_rumah/detail_rumah/') .$rm->id_rumah ?>" class="btn btn-sm btn-success"><i class="
