b451f3fc027f3b6737ab6b48fd8cfb940c8a55b0
Muhammad Ashif Sa'bani <muhashif8@gmail.com>
2021-11-04 16:00:29 +0700

    ganti faq mbek tentang

--- application/views/customer/tentang.php
+++ application/views/customer/tentang.php
@@ -31,7 +31,7 @@
 
         <!-- about #1
 ============================================= -->
-        <section id="about" class="about bg-white">
+        <section id="accordion" class="bg-white">
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12 col-sm-5 col-md-5">
@@ -39,20 +39,44 @@
                     </div>
                     <!-- .col-md-5 -->
                     <div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
-                        <div class="heading heading-3">
-                            <h2 class="heading--title">Visi dan Misi</h2>
-                        </div>
+                            <div class="heading heading-3">
+                            <h2 class="heading--title">Tentang Web Perumahan</h2>
+                        </div><hr>
                         <!-- .heading-title end -->
-                        <div class="about--panel">
-                            <h3>Visi Kami</h3>
-                            <p>Kami menyediakan jasa promosi dalam bentuk web yang bisa diakses untuk semua orang dan yang pastinya hemat biaya.</p>
-                        </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-7 col-md-7">
+                                    <div class="accordion accordion-1" id="accordion">
+                            <div class="panel">
+                                <div class="panel--heading">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse01-1">Apa itu Web Perumahan ?</a>
+                                </div>
+                                <div id="collapse01-1" class="panel--body panel-collapse collapse out">
+                                    <p style="font-family: verdana;">Yaitu website yang dikelola oleh CV. Web Perumahan Team yang bekerja untuk memasarkan berbagai model rumah yang ada di Indonesia.</p>
+                                </div>
+                            </div>
+
+
+                        <div class="panel">
+                                <div class="panel--heading">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse01-2">Visi</a>
+                                </div>
+                                <div id="collapse01-2" class="panel--body panel-collapse collapse out">
+                                    <p style="font-family: verdana;">Kami menyediakan jasa promosi dalam bentuk web yang bisa diakses untuk semua orang dan yang pastinya hemat biaya.</p>
+                                </div>
+                            </div>
                         <!-- .about-panel end -->
-                        <div class="about--panel">
-                            <h3>Tujuan Kami</h3>
-                            <p>Membantu customer yang sedang mencari tempat tinggal yang nyaman dengan promosi yang kami sediakan di website kami ini </p>
-                        </div>
+                       <div class="panel">
+                                <div class="panel--heading">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse01-3">Misi</a>
+                                </div>
+                                <div id="collapse01-3" class="panel--body panel-collapse collapse out">
+                                    <p style="font-family: verdana;">Membantu customer yang sedang mencari tempat tinggal yang nyaman dengan promosi yang kami sediakan di website kami ini</p>
+                                </div>
+                            </div>
                         <!-- .about-panel end -->
+                    </div>
+                    </div>
+                    </div>
                     </div>
                     <!-- .col-md-6 -->
                 </div>
